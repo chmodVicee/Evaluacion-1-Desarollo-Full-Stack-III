22b94facaf45c84b45ee855855077e6e327b5e60
chmodVicee <vicealvarezzz@gmail.com>
2026-04-02 12:25:23 -0300

    Backend Base

---
+++ SmartLogix/src/main/java/com/SmartLogix/SmartLogixApplication.java
@@ -0,0 +1,13 @@
+package com.SmartLogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogixApplication.class, args);
+	}
+
+}
---
+++ SmartLogix/src/test/java/com/SmartLogix/SmartLogixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartLogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
