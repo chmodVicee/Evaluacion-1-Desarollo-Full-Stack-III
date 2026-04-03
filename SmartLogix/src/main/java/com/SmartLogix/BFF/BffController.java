7e6280b72b95bd11d43a4fa844c9e0a9f220ee44
chmodVicee <vicealvarezzz@gmail.com>
2026-04-03 13:39:15 -0300

    Creación de carpeta BFF (Backend For Frontend) con Service, Controller y AppConfig

--- SmartLogix/src/main/java/com/SmartLogix/BFF/BffController.java
+++ SmartLogix/src/main/java/com/SmartLogix/BFF/BffController.java
@@ -1,4 +1,28 @@
 package com.SmartLogix.BFF;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bff")
 public class BffController {
+
+    private final BffService bffService;
+
+    public BffController(BffService bffService) {
+        this.bffService = bffService;
+    }
+
+    @PostMapping("/checkout")
+    public ResponseEntity<String> checkout(@RequestParam String tipo,
+                                           @RequestParam String codigoProducto,
+                                           @RequestParam Integer cantidad) {
+        String response = bffService.createOrderFromFrontend(tipo, codigoProducto, cantidad);
+        return ResponseEntity.ok(response);
+    }
 }
--- SmartLogix/src/main/java/com/SmartLogix/BFF/BffService.java
+++ SmartLogix/src/main/java/com/SmartLogix/BFF/BffService.java
@@ -1,4 +1,28 @@
 package com.SmartLogix.BFF;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class BffService {
+
+    private final RestTemplate restTemplate;
+
+    public BffService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    private static final String ORDER_SERVICE_URL = "http://localhost:8080/api/orders";
+
+    @CircuitBreaker(name = "orderServiceBreaker", fallbackMethod = "orderFallBack")
+    public String createOrderFromFrontend(String tipo, String codigoProducto, Integer cantidad){
+        String url = String.format("%s?tyipo=%&codigoProducto=%s&cantidad=%d",
+                ORDER_SERVICE_URL, tipo, codigoProducto, cantidad);
+        return restTemplate.postForObject(url,null, String.class);
+    }
+
+    public String orderFallBack (String tipo, String productoCodigo, Integer cantidad, Throwable throwable) {
+        return "{ \"error\": \"El servicio de pedidos esta inactivo. Intente en otra ocacion\"}";
+    }
 }
