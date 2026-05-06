e3426b845d7b01ac6572747add29d145dd57162e
chmodVicee <vicealvarezzz@gmail.com>
2026-05-06 18:45:40 -0400

    Refactor: Convertir el proyecto a microservicio exclusivo de Inventory

--- backend-smartLogix/src/main/java/com/SmartLogix/Repository/OrdenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SmartLogix.Repository;
-
-import com.SmartLogix.Model.Orden;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdenRepository extends JpaRepository<Orden, Long> {
-}
