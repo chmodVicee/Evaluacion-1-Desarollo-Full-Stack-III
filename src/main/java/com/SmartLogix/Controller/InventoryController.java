d5ce698a430bed4e59065b7dfe45fe95c745d5b4
chmodVicee <vicealvarezzz@gmail.com>
2026-05-06 21:56:24 -0400

    Add: Se implementó una arquitectura limpia con Service y Handler

--- src/main/java/com/SmartLogix/Controller/InventoryController.java
+++ src/main/java/com/SmartLogix/Controller/InventoryController.java
@@ -1,60 +1,43 @@
 package com.SmartLogix.Controller;
 
-import com.SmartLogix.Model.Inventory; // Asegúrate de que esta ruta coincida con tu paquete real
-import com.SmartLogix.Repository.InventoryRepository;
+import com.SmartLogix.Model.Inventory;
+import com.SmartLogix.Service.InventoryService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
 
-    private final InventoryRepository repository;
+    private final InventoryService service;
 
     @GetMapping("/{productoCodigo}/{almacenCodigo}")
     public ResponseEntity<Inventory> getStock(@PathVariable String productoCodigo, @PathVariable String almacenCodigo) {
-
-        Optional<Inventory> inventarioEncontrado = repository.findByProductoCodigoAndAlmacenCodigo(productoCodigo, almacenCodigo);
-        if (inventarioEncontrado.isPresent()) {
-            return ResponseEntity.ok(inventarioEncontrado.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(service.getStock(productoCodigo, almacenCodigo));
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<Inventory>> getAllStock(){
-        List<Inventory> allInventory = repository.findAll();
-
-        return ResponseEntity.ok(allInventory);
+        return ResponseEntity.ok(service.getAllStock());
     }
 
     @PostMapping("/add")
     public ResponseEntity<Inventory> addProduct(@RequestBody Inventory inventory) {
-        Inventory nuevo = repository.save(inventory);
-        return ResponseEntity.status(201).body(nuevo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addProduct(inventory));
     }
 
     @PostMapping("/bulk-add")
     public ResponseEntity<List<Inventory>> addProducts(@RequestBody List<Inventory> products){
-        List<Inventory> guardados = repository.saveAll(products);
-        return ResponseEntity.status(201).body(guardados);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addProducts(products));
     }
 
     @PostMapping("/update")
     public ResponseEntity<Inventory> updateStock(@RequestBody Inventory request) {
-        Inventory inventory = repository.findByProductoCodigoAndAlmacenCodigo(request.getProductoCodigo(), request.getAlmacenCodigo())
-                .orElse(new Inventory());
-        inventory.setProductoCodigo(request.getProductoCodigo());
-        inventory.setAlmacenCodigo(request.getAlmacenCodigo());
-        inventory.setStock(request.getStock());
-
-        return ResponseEntity.ok(repository.save(inventory));
+        return ResponseEntity.ok(service.updateStock(request));
     }
 }
